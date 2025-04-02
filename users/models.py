ba7fb4dd85386892ab64a9e3ac9c592de5fb7e32
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2025-04-02 21:17:04 +0300

    add class User

--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+NULLABLE = {"blank": True, "null": True}
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name="email")
+    phone = models.CharField(max_length=35, unique=True, verbose_name="phone", **NULLABLE)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+        ordering = ["id"]
