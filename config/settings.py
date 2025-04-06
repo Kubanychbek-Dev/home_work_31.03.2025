05ec24a0d3f3498dbf81b87e1978fff0317dbb44
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2025-04-06 20:50:43 +0300

    add model breed

--- config/settings.py
+++ config/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 
     #Project apps
     'users',
-    'dogs'
+    'dogs',
 
 ]
 
--- dogs/models.py
+++ dogs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Breed(models.Model):
+    name = models.CharField(max_length=100, verbose_name="name")
+    description = models.CharField(max_length=800, verbose_name="description")
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = "breed"
+        verbose_name_plural = "breeds"
