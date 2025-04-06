6809d87a06f1ab885a1cb8948a065ae50a290866
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2025-04-06 21:53:12 +0300

    add Breed and Dog Admins

--- dogs/models.py
+++ dogs/models.py
@@ -2,25 +2,25 @@ from django.db import models
 from users.models import NULLABLE
 
 class Breed(models.Model):
-    name = models.CharField(max_length=100, verbose_name="name")
-    description = models.CharField(max_length=800, verbose_name="description")
+    name = models.CharField(max_length=100, verbose_name="Name")
+    description = models.CharField(max_length=800, verbose_name="Description")
 
     def __str__(self):
         return f"{self.name}"
 
     class Meta:
-        verbose_name = "breed"
-        verbose_name_plural = "breeds"
+        verbose_name = "Breed"
+        verbose_name_plural = "Breeds"
 
 
 class Dog(models.Model):
-    name = models.CharField(max_length=100, verbose_name="dog name")
-    breed = models.ForeignKey(Breed, on_delete=models.CASCADE, verbose_name="breed")
-    img = models.ImageField(upload_to="dogs/", **NULLABLE, verbose_name="image")
+    name = models.CharField(max_length=100, verbose_name="Dog name")
+    breed = models.ForeignKey(Breed, on_delete=models.CASCADE, verbose_name="Dog Breed")
+    img = models.ImageField(upload_to="dogs/", **NULLABLE, verbose_name="Image")
 
     def __str__(self):
         return f"{self.name} ({self.breed})"
 
     class Meta:
-        verbose_name = "dog"
-        verbose_name_plural = "dogs"
+        verbose_name = "Dog"
+        verbose_name_plural = "Dogs"
