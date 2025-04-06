8d4593af2f757033eb212e7fd14adede78ad24b5
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2025-04-06 22:50:33 +0300

    add index_view

--- dogs/views.py
+++ dogs/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from . models import Breed
 
-# Create your views here.
+
+def index_view(request):
+    context = {
+        "object_list": Breed.objects.all(),
+        "title": "PetShop - Главное"
+    }
+    return render(request, 'dogs/index.html', context=context)
