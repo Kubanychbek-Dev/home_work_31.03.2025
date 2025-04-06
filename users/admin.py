37508047f568f29f524c0aba76aafea7e04426c4
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2025-04-06 20:39:15 +0300

    update

--- users/admin.py
+++ users/admin.py
@@ -6,5 +6,5 @@ from users.models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("last_name", "first_name", "email", "is_superuser")
+    list_display = ("last_name", "first_name", "email", "pk")
     list_filter = ("last_name",)
