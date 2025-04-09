99659c48a35de05c4159851058906f1735e60b3d
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2025-04-09 15:24:19 +0300

    add dogs_list and dogs_create

--- dogs/urls.py
+++ dogs/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .apps import DogsConfig
-from .views import index_view, breeds_list_view, breed_dogs_list_view
+from .views import index_view, breeds_list_view, breed_dogs_list_view, dogs_list_view, dog_create_view
 
 app_name = DogsConfig.name
 
@@ -9,4 +9,6 @@ urlpatterns = [
     path('', index_view, name='index'),
     path('breeds/', breeds_list_view, name='breeds'),
     path('breeds/<int:pk>/dogs/', breed_dogs_list_view, name='breed_dogs'),
+    path('dogs/', dogs_list_view, name='dogs_list'),
+    path('dogs/create/', dog_create_view, name='dog_create'),
 ]
