dbfaac073ff1e24235a4d8700c36b6a664c1fa1b
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2025-04-09 15:23:29 +0300

    add dogs_list_view and dog_create_view

--- dogs/views.py
+++ dogs/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 from . models import Breed, Dog
+from .forms import DogForm
 
 
 def index_view(request):
@@ -26,3 +30,25 @@ def breed_dogs_list_view(request, pk: int):
         "breed_pk": breed_obj.pk
     }
     return render(request, 'dogs/dogs.html', context=context)
+
+
+def dogs_list_view(request):
+    context = {
+        "object_list": Dog.objects.all(),
+        "title": "Все собаки",
+    }
+    return render(request, 'dogs/dogs.html', context=context)
+
+
+def dog_create_view(request):
+    if request.method == "POST":
+        form = DogForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("dogs:dogs_list"))
+
+    context = {
+        "title": "Добавить собаку",
+        "form": DogForm
+    }
+    return render(request, "dogs/create.html", context=context)
