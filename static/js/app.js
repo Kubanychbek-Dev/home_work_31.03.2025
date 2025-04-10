09ebb6e4276d17731e2fa796279c2c132b05d476
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2025-04-10 15:24:01 +0300

    update

---
+++ static/js/app.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", () => {
+  document.querySelector(".header-btn").addEventListener("click", () => {
+    document.querySelector(".menu").classList.toggle("menu--show")
+  })
+})
