0420825799e4f2feaa24200d7acd4843c885adaf
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2025-04-12 17:12:02 +0300

    add new fields to users/model

--- users/models.py
+++ users/models.py
@@ -7,7 +7,11 @@ NULLABLE = {"blank": True, "null": True}
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name="email")
+    first_name = models.CharField(max_length=100, verbose_name="First Name", default="Not specified")
+    last_name = models.CharField(max_length=100, verbose_name="Last Name", default="Not specified")
     phone = models.CharField(max_length=35, unique=True, verbose_name="phone", **NULLABLE)
+    avatar = models.ImageField(upload_to="users/", verbose_name="Avatar", **NULLABLE)
+    is_active = models.BooleanField(default=True, verbose_name="Activity")
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
