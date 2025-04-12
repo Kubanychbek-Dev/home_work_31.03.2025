0647a2bfd972dd659bc7e03a67281b1b5eaa7901
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2025-04-12 18:48:09 +0300

    update in create_super_user.py

--- users/management/commands/create_super_user.py
+++ users/management/commands/create_super_user.py
@@ -5,12 +5,12 @@ from users.models import User
 class Command(BaseCommand):
     def handle(self, *args, **options):
         admin_user = User.objects.create(
-            email="user@mail.com",
-            first_name="Spongebob",
-            last_name="Squerpants",
+            email="user_stark@mail.com",
+            first_name="Tony",
+            last_name="Stark",
             is_staff=True,
             is_active=True,
             is_superuser=True
         )
-        admin_user.set_password("user")
+        admin_user.set_password("stark")
         admin_user.save()
