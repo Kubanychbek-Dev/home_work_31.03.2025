66f505fe6a008192afeafe2c943aca183f091405
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2025-04-18 13:56:42 +0300

    add user_generate_new_password and send_register_email

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from users.apps import UsersConfig
-from .views import user_register_view, user_login_view, user_profile_view, user_logout_view, user_update_view, user_change_password_view
+from .views import user_register_view, user_login_view, user_profile_view, user_logout_view, user_update_view, user_change_password_view, user_generate_new_password
 
 app_name = UsersConfig.name
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path("profile/", user_profile_view, name="user_profile"),
     path("update/", user_update_view, name="user_update"),
     path("change_password/", user_change_password_view, name="user_change_password"),
+    path("profile/genpassword/", user_generate_new_password, name="user_generate_new_password"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,6 @@
+import string
+import random
+
 from django.shortcuts import render, reverse, redirect
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
@@ -5,6 +8,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 from .forms import UserRegisterForm, UserLoginForm, UserUpdateForm, UserChangePasswordForm
+from .services import send_register_email, send_new_password
 
 
 def user_register_view(request):
@@ -12,10 +16,9 @@ def user_register_view(request):
     if request.method == "POST":
         if form.is_valid():
             new_user = form.save()
-            # print(new_user)
-            # print(form.cleaned_data["password"])
             new_user.set_password(form.cleaned_data["password"])
             new_user.save()
+            send_register_email(new_user.email)
             return HttpResponseRedirect(reverse("users:user_login"))
     context = {
         "title": "Create account",
@@ -95,3 +98,12 @@ def user_change_password_view(request):
 def user_logout_view(request):
     logout(request)
     return redirect("dogs:index")
+
+
+@login_required(login_url="users:user_login")
+def user_generate_new_password(request):
+    new_password = "".join(random.sample((string.ascii_letters + string.digits), 12))
+    request.user.set_password(new_password)
+    request.user.save()
+    send_new_password(request.user.email, new_password)
+    return redirect(reverse("dogs:index"))
