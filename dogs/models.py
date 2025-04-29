62ce5e09f865cbc7a77367faab0a23ff8c0d911e
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2025-04-29 17:37:59 +0300

    add DogParent model

--- dogs/models.py
+++ dogs/models.py
@@ -27,3 +27,17 @@ class Dog(models.Model):
     class Meta:
         verbose_name = "Dog"
         verbose_name_plural = "Dogs"
+
+
+class DogParent(models.Model):
+    dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, verbose_name="Parent's name")
+    breed = models.ForeignKey(Breed, on_delete=models.CASCADE, verbose_name="Parent's breed")
+    birth_date = models.DateField(**NULLABLE, verbose_name="Parent's birthdate")
+
+    def __str__(self):
+        return f"{self.name} ({self.breed})"
+
+    class Meta:
+        verbose_name = "Parent"
+        verbose_name_plural = "Parents"
