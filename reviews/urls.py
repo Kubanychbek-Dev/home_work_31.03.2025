9586294059274822cda0ab1c024bffcc7242319c
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2025-05-04 13:30:24 +0300

    add reviews

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import ReviewListView, ReviewDeactivatedListView, ReviewCreateView, ReviewDetailView, ReviewUpdateView, ReviewDeleteView
+from .views import (ReviewListView, ReviewDeactivatedListView, ReviewCreateView, ReviewDetailView,
+                    ReviewUpdateView, ReviewDeleteView, review_toggle_activity)
 
 from .apps import ReviewsConfig
 app_name = ReviewsConfig.name
@@ -8,7 +9,8 @@ urlpatterns = [
     path("", ReviewListView.as_view(), name="reviews_list"),
     path("deactivated/", ReviewDeactivatedListView.as_view(), name="reviews_deactivated_list"),
     path("create/", ReviewCreateView.as_view(), name="review_create"),
-    path("detail/", ReviewDetailView.as_view(), name="review_detail"),
-    path("update/", ReviewUpdateView.as_view(), name="review_update"),
-    path("deactivated/", ReviewDeleteView.as_view(), name="review_delete"),
+    path("detail/<slug:slug>/", ReviewDetailView.as_view(), name="review_detail"),
+    path("update/<slug:slug>/", ReviewUpdateView.as_view(), name="review_update"),
+    path("deactivated/<slug:slug>/", ReviewDeleteView.as_view(), name="review_delete"),
+    path("toggle/<slug:slug>/", review_toggle_activity, name="review_toggle"),
 ]
