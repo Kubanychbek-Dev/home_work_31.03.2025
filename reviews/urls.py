6ca51c4afaf8f90cb674077e48838d5533d5dcf7
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2025-05-04 12:02:22 +0300

    add reviews

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
+from .views import ReviewListView, ReviewDeactivatedListView
 from .apps import ReviewsConfig
 app_name = ReviewsConfig.name
 
 urlpatterns = [
-
+    path("", ReviewListView.as_view(), name="reviews_list"),
+    path("deactivated/", ReviewDeactivatedListView.as_view(), name="reviews_deactivated_list"),
 ]
