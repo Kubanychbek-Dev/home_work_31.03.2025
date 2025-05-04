4e8a0afd2398db9c5dd8c5ed0df4f9b2295fdfe6
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2025-05-04 13:08:51 +0300

    add CRUD for reviews

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import ReviewListView, ReviewDeactivatedListView
+from .views import ReviewListView, ReviewDeactivatedListView, ReviewCreateView, ReviewDetailView, ReviewUpdateView, ReviewDeleteView
+
 from .apps import ReviewsConfig
 app_name = ReviewsConfig.name
 
 urlpatterns = [
     path("", ReviewListView.as_view(), name="reviews_list"),
     path("deactivated/", ReviewDeactivatedListView.as_view(), name="reviews_deactivated_list"),
+    path("create/", ReviewCreateView.as_view(), name="review_create"),
+    path("detail/", ReviewDetailView.as_view(), name="review_detail"),
+    path("update/", ReviewUpdateView.as_view(), name="review_update"),
+    path("deactivated/", ReviewDeleteView.as_view(), name="review_delete"),
 ]
